6fa97b8158a7766c4b6c342b5fa8e3796efe0f13
San Yung <sankung93@gmail.com>
2020-12-18 09:15:35 -0800

    correctly took out logger

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -12,7 +12,7 @@ import thunk from 'redux-thunk';
 const configureStore = (preloadedState = {}) => {
     let middleware = [thunk];
     // if (process.env.NODE_ENV !== 'production') {
-    //     middleware = [...middleware, logger];
+        middleware = [...middleware, logger];
     // }
     return createStore(
         rootReducer,
