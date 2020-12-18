e5d9f624c56f8b12626e63f457006a475cef4cca
San Yung <sankung93@gmail.com>
2020-12-17 22:37:47 -0800

    finished action cable

--- frontend/components/posts/post_index.jsx
+++ frontend/components/posts/post_index.jsx
@@ -9,7 +9,6 @@ class PostIndex extends React.Component {
 
         //action cable
         this.state = { posts: [] };
-        // this.bottom = React.createRef();
     }
 
     //action cable
@@ -27,10 +26,12 @@ class PostIndex extends React.Component {
         );
         this.props.fetchPosts(this.props.channelId),
         this.props.fetchUsers()
+        this.scrollToBottom();
     }
 
 
     componentDidUpdate(previousProps){
+        this.scrollToBottom();
         if (previousProps.match.params.channelId !== this.props.channelId){
             this.props.fetchPosts(this.props.channelId)
         }
@@ -40,6 +41,23 @@ class PostIndex extends React.Component {
         // );
     }
 
+    scrollToBottom() {
+        this.postEnd.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    getTime(string){
+        var date = new Date(string)
+        let hours = date.getHours()
+        let minutes = date.getMinutes()
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12
+        hours = hours ? hours : 12; 
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var strTime = hours + ':' + minutes + ' ' + ampm;
+        return strTime;
+    }
+
+
     render() {
         return (
             <div id="postindex">
@@ -47,7 +65,7 @@ class PostIndex extends React.Component {
                 {this.props.posts.map((post) => (
                     <div className="postList" key={post.id}>
 
-                    { !this.props.users[post.user_id] ?  'e2d2' :
+                    { !this.props.users[post.user_id] ?  null :
                         <div className="postList">
                             <img id="demoprofile" src={window.profileURL} />
                             <br></br>
@@ -56,22 +74,26 @@ class PostIndex extends React.Component {
 
                             <div id="authorstuff">
                                 <div id="author">{this.props.users[post.user_id].username}</div> &nbsp;&nbsp; 
-                                <div id="timestamp">{post.created_at}</div>
+                                {/* <div id="timestamp">{ Array.from(post.created_at).slice(11,16) }</div> */}
+                                        <div id="timestamp">{ this.getTime(post.created_at)}</div>
                             </div>
 
                              <br></br> 
                              
                             <div id="message">{post.body}</div>
 
-                        {/* <div ref={ (ele) => this.bottom = ele} />  */}
-
-                            </div>
-                        </div>
+                        </div  > 
+                    </div>
                         
                     }
+
                     
                     </div>
+                    
                 ))}
+                <div ref={el => {this.postEnd = el}}></div>
+
+                
     
                 <div className="postform-container" >
                     < PostCreateContainer channelId={this.props.channelId} channel={this.props.channel}/>
