183355722f35123760e16076375a90bdcc6bc329
San Yung <sankung93@gmail.com>
2020-12-18 09:12:16 -0800

    took out logger

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import userReducer from "./user_reducer";
 import channelReducer from "./channel_reducer";
-import postReducer from "./post_reducer"
+import postReducer from "./post_reducer";
 
 const entitiesReducer = combineReducers({
     users: userReducer,
