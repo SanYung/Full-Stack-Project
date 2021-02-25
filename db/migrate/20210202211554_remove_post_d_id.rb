df05d424fa56f883b601c4e0e7da921dca99e01f
San Yung <sankung93@gmail.com>
2021-02-24 16:33:28 -0800

    removed logger

--- db/migrate/20210202211554_remove_post_d_id.rb
+++ db/migrate/20210202211554_remove_post_d_id.rb
@@ -2,6 +2,6 @@ class RemovePostDId < ActiveRecord::Migration[5.2]
   def change
     remove_column :posts, :dmessage_id
     remove_column :posts, :channel_id
-    add_column :posts, :channel_id, :integer, null:false
+    add_column :posts, :channel_id, :integer
   end
 end
